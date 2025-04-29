df886a929cfba7f094507c2655f9fa8a0d7f1bdb
vasyakohotyk <kogotukvasil@gmail.com>
2025-04-29 18:04:27 +0300

    first commidt

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'ServicePro - Drivers license',
   description: 'Ваш путь к званию водителя',
+  generator: 'v0.dev',
 }
 
 export default function RootLayout({
