2d7741bf3d892584fcc97df7b16ed31d66b7fcf9
vasyakohotyk <kogotukvasil@gmail.com>
2025-04-29 18:08:43 +0300

    first commidt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,33 @@
 import type { Metadata } from 'next'
 import './globals.css'
 
+import type { Metadata } from 'next'
+import './globals.css'
+
 export const metadata: Metadata = {
-  title: 'ServicePro - Drivers license',
-  description: 'Ваш путь к званию водителя',
-  generator: 'v0.dev',
+  title: 'ServicePro — Водительские права',
+  description: 'Оформление водительских прав в России быстро и надёжно.',
+  generator: 'Next.js',
+  keywords: ['водительские права', 'оформление прав', 'ServicePro'],
+  creator: 'ServicePro',
+  metadataBase: new URL('https://drive-license-six.vercel.app'),
+  openGraph: {
+    title: 'ServicePro — Водительские права',
+    description: 'Легальное оформление прав в РФ ',
+    url: 'https://drive-license-six.vercel.app',
+    siteName: 'ServicePro',
+    images: [
+      {
+      
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'ru_RU',
+    type: 'website',
+  },
 }
+
 
 export default function RootLayout({
   children,
